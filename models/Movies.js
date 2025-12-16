c17659f13fa44169e4a8dce772a22f3134987f4b
akash-912 <akashdeep12896@gmail.com>
2025-12-16 15:09:51 +0530

    first commit

---
+++ models/Movies.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+// Define Schema
+
+const movieSchema = new mongoose.Schema({
+  name: {type:String,required:true, trim: true},
+  ratings: {type: Number,required:true,min:1,max:5},
+  money: {
+    type: mongoose.Decimal128,
+    required: true,
+    validate: v => v => 10,
+  },
+  genre: {type:Array},
+  isActive: {type:Boolean},
+  comments: [{value: {type:String},published: {type: Date, default: Date.now}}],
+})
+
+// Creating model
+
+
+const MovieModel = mongoose.model('Movie',movieSchema);
+
+
+const createDoc = async () => {
+  try{
+    const m1 = new MovieModel({
+      name: "Extraction 2",
+      ratings: 4,
+      money: 60000,
+      genre: ['action','adventure'],
+      isActive: true,
+      comments: [{value: "That was an amazing movie."}],
+    })
+    const result = await m1.save()
+    console.log(result)
+  }catch(error){
+    console.log(error)
+  }
+}
+
+
+export {createDoc};
